5c47def3ed5839ed5b86a1d6abf9f125342fd724
SabaRuk <sabarukhadze2021@gmail.com>
2024-09-28 22:20:32 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
 import React from 'react';
-import Navbar from "./Components/Navbar";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 import WorldMap from './Components/WorldMap';
 import UniversitySearch from './Components/UniversitySearch';
 import BottomNavbar from './Components/BottomNavbar';
 
 function App() {
-  let component;
-  switch (window.location.pathname) {
-    case "/":
-    case "/Home": // Handles both "/" and "/Home" for the home page
-      component = (
-        <React.Fragment>
-          <Home />
-          <WorldMap />
-          <UniversitySearch />
-        </React.Fragment>
-      );
-      break;
-    case "/About":
-      component = <About />;
-      break;
-    case "/Contact":
-      component = <Contact />;
-      break;
-    default:
-      component = (
-        <React.Fragment>
-          <Home />
-          <WorldMap />
-          <UniversitySearch />
-        </React.Fragment>
-      ); // Default to the home page if the path doesn't match
-  }
-
   return (
-    <React.Fragment>
+    <Router>
       <Navbar />
-      {component}
+      
+      {/* Defining Routes for Different Pages */}
+      <Routes>
+        {/* Home Route */}
+        <Route 
+          path="/" 
+          element={
+            <React.Fragment>
+              <Home />
+              <WorldMap />
+              <UniversitySearch />
+            </React.Fragment>
+          } 
+        />
+        
+        {/* About Route */}
+        <Route path="/about" element={<About />} />
+        
+        {/* Contact Route */}
+        <Route path="/contact" element={<Contact />} />
+        
+        {/* Catch-all Route: Default Route */}
+        <Route 
+          path="*" 
+          element={
+            <React.Fragment>
+              <Home />
+              <WorldMap />
+              <UniversitySearch />
+            </React.Fragment>
+          } 
+        />
+      </Routes>
+      
+      {/* Footer Navbar */}
       <BottomNavbar />
-    </React.Fragment>
+    </Router>
   );
 }
 
